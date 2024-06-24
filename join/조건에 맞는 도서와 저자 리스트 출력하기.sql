0c800ceb0a5b43956b4d216054fd6526a8e0e46c
Jae-Young98 <ygy0102@gmail.com>
2024-06-24 20:03:36 +0900

    조건에 맞는 도서와 저자 리스트 출력하기

---
+++ join/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,5 @@
+SELECT BOOK_ID, AUTHOR_NAME, date_format(PUBLISHED_DATE, '%Y-%m-%d') as PUBLISHED_DATE
+from book
+         join author on book.author_id = author.author_id
+where book.category = '경제'
+order by published_date
